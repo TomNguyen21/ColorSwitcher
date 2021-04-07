4221e4fce6bf4e01eb8bf4f65a65c16459f02885
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-04-06 18:25:07 -0700

    Add Hex Color logic

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ btn.addEventListener('click', () => {
 const getRandomNum = () => {
   return Math.floor(Math.random() * colors.length);
 }
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn2 = document.getElementById("btn2");
+
+btn2.addEventListener('click', () => {
+  let random = getRandomNum();
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[random];
+  }
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+})
